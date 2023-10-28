0f6fdae0482ff7a23e7b621e8a3501ed1f0d3823
KARPED1EM <leever.zzz@gmail.com>
2023-10-29 07:58:32 +0800

    Add JumpToVersionButton In Play Page

--- NAUL/Pages/PlayPage.xaml.cs
+++ NAUL/Pages/PlayPage.xaml.cs
@@ -16,4 +16,7 @@ public sealed partial class Page_Play : Page
     {
         this.InitializeComponent();
     }
+
+    private void JumpToVersionButton_Click(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        => Page_Main.Current.NavigateTo(typeof(Page_Version));
 }
