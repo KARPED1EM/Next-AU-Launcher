333ff9dca73eb7ea7e535cab636b5f27f7bcc137
KARPED1EM <leever.zzz@gmail.com>
2023-10-28 22:05:01 +0800

    Add CloudService.cs & VersionService.cs

---
+++ NAUL/Services/CloudService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAUL.Services;
+
+internal class CloudService
+{
+
+}
