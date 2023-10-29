6d83bb83579bf9d612f9653227a33f74c37a2bee
KARPED1EM <leever.zzz@gmail.com>
2023-10-29 20:04:42 +0800

    Add AssemblyInfoItem Class

---
+++ NAUL/Models/AssemblyInfoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NAUL.Models;
+
+public class AssemblyInfoItem
+{
+    public string MD5 { get; set; }
+
+    public GamePlatforms Platform { get; set; }
+
+    public Version Version { get; set; }
+
+}
