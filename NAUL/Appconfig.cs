8f659bdb90a7b8e33aa7c3c6541eacd3eb1397f2
KARPED1EM <leever.zzz@gmail.com>
2023-10-29 21:26:24 +0800

    Add Appconfig Class

---
+++ NAUL/Appconfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAUL;
+
+internal class Appconfig
+{
+    public static string CONFIG_SAVE_PATH => Environment.GetFolderPath(0) + "/NAUL_Config";
+}
