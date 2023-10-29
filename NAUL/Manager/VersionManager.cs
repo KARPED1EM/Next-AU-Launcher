e73da99405ffb9477a3a219918d84fc67b88dd09
KARPED1EM <leever.zzz@gmail.com>
2023-10-29 23:51:31 +0800

    Add Version Init And Json IO

--- NAUL/Manager/VersionManager.cs
+++ NAUL/Manager/VersionManager.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.IO;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
+using System.Text.Json;
 using NAUL.Models;
 using NAUL.Services;
+using Newtonsoft.Json.Linq;
 
 namespace NAUL.Manager;
 
@@ -18,10 +15,53 @@ internal class VersionManager
     private static VersionItem _SelectedVersion;
     public static VersionItem SelectedVersion { get { return _SelectedVersion ?? Versions.FirstOrDefault(); } set { _SelectedVersion = value; } }
 
-    public static Version GetModVersion(string path)
+    public static void Init()
     {
-        var version = FileVersionInfo.GetVersionInfo(path).FileVersion;
-        return Version.Parse(version);
+        // Read json file if exists
+        if (File.Exists(DataPaths.CONFIG_Version_FILE))
+        {
+            string jsonString = System.IO.File.ReadAllText(DataPaths.CONFIG_Version_FILE);
+
+            JArray jarray = JArray.Parse(jsonString);
+            foreach(var js in jarray.ToList())
+            {
+                VersionItem item = JsonSerializer.Deserialize<VersionItem>(js.ToString());
+                if (item == null) continue;
+                if (!Versions.Any(v => v.Path == item.Path))
+                    Versions.Add(item);
+            }
+        }
+
+        // Load from GameFinder
+        FindGameService.SearchAllByRegistry();
+        bool needSave = false;
+        foreach(var path in FindGameService.FoundGamePaths.Where(p => !Versions.Any(v => v.Path == p)))
+        {
+            VersionItem item = new()
+            {
+                DisplayGlyph = "\uE7FC",
+                Path = path
+            };
+            item.DisplayName = item.GamePlatform switch
+            {
+                GamePlatforms.Steam => "Steam",
+                GamePlatforms.Epic => "Epic",
+                _ => "本地",
+            };
+
+            Versions.Add(item);
+            needSave = true;
+        }
+        if (needSave) SaveVersionsToConfig();
+
+
     }
 
+    public static void SaveVersionsToConfig()
+    {
+        string jsonString = JsonSerializer.Serialize(Versions);
+        if (!File.Exists(DataPaths.CONFIG_Version_FILE))
+            System.IO.File.Create(DataPaths.CONFIG_Version_FILE).Close();
+        System.IO.File.WriteAllText(DataPaths.CONFIG_Version_FILE, jsonString);
+    }
 }
