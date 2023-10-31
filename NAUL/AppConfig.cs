024091b890decf35b46a64e3f86464c787740f14
KARPED1EM <leever.zzz@gmail.com>
2023-11-01 02:48:20 +0800

    Add AppConfig Class

--- NAUL/AppConfig.cs
+++ NAUL/AppConfig.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.IO;
 
 namespace NAUL;
@@ -26,4 +27,36 @@ public static class DataPaths
         Directory.CreateDirectory(SAVE_CONFIG_PATH);
         Directory.CreateDirectory(SAVE_PLUGIN_PATH);
     }
+}
+
+public static class AppConfig
+{
+    private static RegistryKey SoftwareKeys => Registry.CurrentUser.OpenSubKey("Software", true);
+    private static RegistryKey _Keys;
+    public static RegistryKey Keys
+    {
+        get
+        {
+            _Keys ??= SoftwareKeys.OpenSubKey("Next-AU-Launcher", true);
+            return _Keys;
+        }
+        set { _Keys = value; }
+    }
+
+    public static T GetValue<T> (string key)
+    {
+        var value = Keys.GetValue(key);
+        return (T)value ?? default;
+    }
+
+    public static void SetValue (string key, object value)
+    {
+        Keys.SetValue(key, value);
+    }
+}
+
+public enum AppConfigs
+{
+    DataPath,
+    SelectedVersion,
 }
