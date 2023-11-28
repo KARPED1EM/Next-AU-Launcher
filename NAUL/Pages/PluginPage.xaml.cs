f7631f3bc40778ba816708be8bf966930fc18b7f
KARPED1EM <leever.zzz@gmail.com>
2023-11-28 16:28:34 +0800

    Update Plugin Page

--- NAUL/Pages/PluginPage.xaml.cs
+++ NAUL/Pages/PluginPage.xaml.cs
@@ -8,6 +8,8 @@ namespace NAUL;
 
 public sealed partial class Page_Plugin : Page
 {
+    public static Page_Plugin Current { get; private set; }
+
     private ObservableCollection<VersionItem> VersionItemsSource
         => VersionManager.AllVersions.ToObservableCollection();
     private ObservableCollection<PluginItem> SinglePluginItemsSource
@@ -15,10 +17,10 @@ public sealed partial class Page_Plugin : Page
     private ObservableCollection<PluginItem> AdditionalPluginItemsSource
         => PluginManager.AllAdditionalPlugins.ToObservableCollection();
 
-    private object SelectedVersion = VersionManager.SelectedVersion;
-
     public Page_Plugin()
     {
+        Current = this;
+
         this.InitializeComponent();
     }
 
@@ -34,6 +36,41 @@ public sealed partial class Page_Plugin : Page
 
     private void AdditionalPluginItemsCheckBox_Checked(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
     {
-
+        var plugin = PluginManager.AllPlugins.Find(p => p.MD5 == (sender as CheckBox).Tag.ToString());
+        if (plugin == null) return;
+        VersionManager.SelectedVersion.SetPluginStatus(plugin, true);
     }
+
+    private void AdditionalPluginItemsCheckBox_Unchecked(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+    {
+        var plugin = PluginManager.AllPlugins.Find(p => p.MD5 == (sender as CheckBox).Tag.ToString());
+        if (plugin == null) return;
+        VersionManager.SelectedVersion.SetPluginStatus(plugin, false);
+    }
+
+    private void SelectVersionComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        var comboBox = sender as ComboBox;
+        var addedItem = (VersionItem)e.AddedItems.FirstOrDefault();
+        var removedItem = (VersionItem)e.RemovedItems.FirstOrDefault();
+
+        if (!e.RemovedItems.Any()
+            || !e.AddedItems.Any()
+            || addedItem == removedItem
+            || addedItem == VersionManager.SelectedVersion) return;
+
+        if (!addedItem.HasBepInExInstalled)
+        {
+            _ = Page_Dialog.Create("该版本未安装BepInEx", "游戏必须安装BepInEx以使用模组").ShowAsync();
+            comboBox.SelectedItem = removedItem;
+            return;
+        }
+
+        VersionManager.SelectedVersion = (e.AddedItems.FirstOrDefault() as VersionItem);
+        Bindings.Update();
+        comboBox.SelectedItem = VersionManager.SelectedVersion;
+    }
+
+    private void SelectVersionComboBox_Loaded(object sender, Microsoft.UI.Xaml.RoutedEventArgs e)
+        => (sender as ComboBox).SelectedItem = VersionManager.SelectedVersion;
 }
