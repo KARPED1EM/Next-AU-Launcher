1f24c3369e92b7a1e8483a8bcad27664639e03f0
KARPED1EM <leever.zzz@gmail.com>
2023-11-28 15:30:47 +0800

    File Classification And Organization

---
+++ NAUL/Utils/AppConfig.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Win32;
+
+namespace NAUL;
+
+public static class AppConfig
+{
+    private static RegistryKey SoftwareKeys => Registry.CurrentUser.OpenSubKey("Software", true);
+    private static RegistryKey _Keys;
+    public static RegistryKey Keys
+    {
+        get
+        {
+            _Keys ??= SoftwareKeys.OpenSubKey("Next-AU-Launcher", true);
+            return _Keys;
+        }
+        set { _Keys = value; }
+    }
+
+    public static T GetValue<T>(string key)
+    {
+        var value = Keys.GetValue(key);
+        return (T)value ?? default;
+    }
+
+    public static void SetValue(string key, object value)
+    {
+        Keys.SetValue(key, value);
+    }
+}
+
+public enum AppConfigs
+{
+    DataPath,
+    SelectedVersion,
+}
--- NAUL/Utils/DataPaths.cs
+++ NAUL/Utils/DataPaths.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.Win32;
-using System.IO;
+﻿using System.IO;
 
 namespace NAUL;
 
@@ -26,36 +25,4 @@ public static class DataPaths
         Directory.CreateDirectory(SAVE_CONFIG_PATH);
         Directory.CreateDirectory(SAVE_PLUGIN_PATH);
     }
-}
-
-public static class AppConfig
-{
-    private static RegistryKey SoftwareKeys => Registry.CurrentUser.OpenSubKey("Software", true);
-    private static RegistryKey _Keys;
-    public static RegistryKey Keys
-    {
-        get
-        {
-            _Keys ??= SoftwareKeys.OpenSubKey("Next-AU-Launcher", true);
-            return _Keys;
-        }
-        set { _Keys = value; }
-    }
-
-    public static T GetValue<T>(string key)
-    {
-        var value = Keys.GetValue(key);
-        return (T)value ?? default;
-    }
-
-    public static void SetValue(string key, object value)
-    {
-        Keys.SetValue(key, value);
-    }
-}
-
-public enum AppConfigs
-{
-    DataPath,
-    SelectedVersion,
 }
